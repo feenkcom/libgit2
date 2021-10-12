6d88bfe40aa52a36040dc06ccf7df9bb25e51c2c
Aliaksei Syrel <alex.syrel@gmail.com>
2021-10-12 12:43:32 +0200

    integrate with shared lib builder

---
+++ shared-library-builder/builder/src/main.rs
@@ -0,0 +1,23 @@
+use libgit2_library::libgit2;
+use shared_library_builder::{Library, LibraryCompilationContext, LibraryTarget};
+use std::error::Error;
+use std::path::Path;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let git2 = libgit2(None as Option<String>);
+
+    let src_path = Path::new("target/src");
+    if !src_path.exists() {
+        std::fs::create_dir_all(&src_path)?;
+    }
+
+    let context = LibraryCompilationContext::new(
+        src_path,
+        "target",
+        LibraryTarget::for_current_platform(),
+        false,
+    );
+    let compiled_git2 = git2.compile(&context)?;
+    println!("Compiled {}", compiled_git2.display());
+    Ok(())
+}
---
+++ shared-library-builder/library/src/lib.rs
@@ -0,0 +1,28 @@
+use libopenssl_library::libcrypto;
+use shared_library_builder::{CMakeLibrary, CompiledLibraryName, GitLocation, LibraryLocation};
+
+pub fn libgit2(binary_version: Option<impl Into<String>>) -> CMakeLibrary {
+    let openssl = libcrypto(None as Option<String>);
+
+    let libssh2 = CMakeLibrary::new(
+        "ssh2",
+        LibraryLocation::Git(GitLocation::github("libssh2", "libssh2").tag("libssh2-1.9.0")),
+    )
+    .define_common("CRYPTO_BACKEND", "OpenSSL")
+    .depends(Box::new(openssl));
+
+    CMakeLibrary::new(
+        "git2",
+        LibraryLocation::Git(
+            GitLocation::github("syrel", "libgit2").branch("v1.1.1-windows-openssl"),
+        ),
+    )
+    .compiled_name(CompiledLibraryName::Matching("git2".to_string()))
+    .define_common("BUILD_CLAR", "OFF")
+    .define_common("REGEX_BACKEND", "builtin")
+    .define_common("USE_BUNDLED_ZLIB", "ON")
+    .depends(Box::new(libssh2))
+    .with_release_location(binary_version.map(|version| {
+        LibraryLocation::Git(GitLocation::github("feenkcom", "libgit2").tag(version))
+    }))
+}
